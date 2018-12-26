13147d5ced50e8b222e52bad21efc16423acdf2a
Nirjan Khadka <webdev.nk@gmail.com>
2018-12-26 19:27:29 +0545

    - fixed the sticky header animation - fixed the scroll to section animation

--- js/main.js
+++ js/main.js
@@ -7,3 +7,26 @@ TOGGLEBTN.addEventListener("click",function () {
     this.classList.toggle("active");
     NAVLINKS.classList.toggle("show");
 });
+
+// smooth scrolling
+
+let scroll = new SmoothScroll('a[href*="#"]',{
+    speed: 300,
+    easing: 'easeOutCubic',
+    header: '#mainHeader'
+});
+
+
+const MAINHEADER = document.querySelector("#mainHeader");
+let headerHeight = MAINHEADER.scrollHeight;
+
+window.addEventListener("scroll",function(){
+    if(this.window.scrollY > headerHeight){
+        MAINHEADER.classList.add("stick");
+        MAINHEADER.classList.remove("unstick");
+    }
+    else{
+        MAINHEADER.classList.remove("stick");
+        MAINHEADER.classList.add("unstick");
+    }
+})
